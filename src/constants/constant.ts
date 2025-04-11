b6958491ef4d95d5f08d3a2f51c91a22ad7168de
phidangnguyen <phidang0210@gmail.com>
2025-04-11 16:27:01 +0700

    update ua tele auth bot

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -4,4 +4,4 @@ export const LUCKY_WHEEL_CONTRACT = '0xaB6Cdd1e0F6C884638C1BfF759EC7D1104ce3713'
 export const LUCKY_WHEEL_CONTRACT_A8_TESTNET = '0x7b5EcFe4D59ba08b85e5230e3E2a2Aca7f476692'
 export const BUNDLER_ENDPOINT = 'https://bundler-stg.layerg.xyz/';
 // export const TELEGRAM_REDIRECT_BOT='https://t.me/layer_g_auth_dev_bot?start=login-3-7c581609293E503dE149d93f34767DFF33d32C16'
-export const TELEGRAM_REDIRECT_BOT='https://t.me/layerg_ua_verification_dev_bot?start=login-3-7c581609293E503dE149d93f34767DFF33d32C16'
+export const TELEGRAM_REDIRECT_BOT='https://t.me/layerg_ua_verification_stg_bot?start=login-3-7c581609293E503dE149d93f34767DFF33d32C16'
