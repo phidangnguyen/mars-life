0ce5f3d6cea9cf71fe98707dcd5b3dbd32fa838a
phidangnguyen <phidang0210@gmail.com>
2025-05-20 16:43:37 +0700

    update config

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,8 +1,8 @@
 
-export const BUNDLER_ENDPOINT = 'http://localhost:3003/api/v3/';
+// export const BUNDLER_ENDPOINT = 'http://localhost:3003/api/v3/';
 export const LUCKY_WHEEL_CONTRACT = '0xaB6Cdd1e0F6C884638C1BfF759EC7D1104ce3713'
 export const LUCKY_WHEEL_CONTRACT_A8_TESTNET = '0x7b5EcFe4D59ba08b85e5230e3E2a2Aca7f476692'
-// export const BUNDLER_ENDPOINT = 'https://bundler-dev.layerg.xyz/api/v3/'
+export const BUNDLER_ENDPOINT = 'https://bundler-dev.layerg.xyz/api/v3/'
 export const TELEGRAM_REDIRECT_BOT='https://t.me/layer_g_auth_dev_bot?start=login-3-DhwmO7MMf1eUyTXwIrqcATdEWx2wtPKhqOQANzrRsCk'
 // export const TELEGRAM_REDIRECT_BOT='https://t.me/layerg_ua_verification_stg_bot?start=login-3-7c581609293E503dE149d93f34767DFF33d32C16'
 export const API_KEY = 'DhwmO7MMf1eUyTXwIrqcATdEWx2wtPKhqOQANzrRsCk'
